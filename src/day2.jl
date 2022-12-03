9e0939b2a41610ea06b327fa2d9789c0743c0a1e
Wolf Thomsen <git@wolthom.com>
2022-12-03 04:02:03 +0100

    finish day2 and improve solutions

--- src/day2.jl
+++ src/day2.jl
@@ -7,6 +7,19 @@ using MLStyle
     Scissors
 end
 
+const game_scores = (
+    (3, 6, 0), # (Rock, X)
+    (0, 3, 6), # (Paper, X)
+    (6, 0, 3), # (Scissors, X)
+)
+
+const hand_map = (
+    (3, 1, 2), # Rock
+    (1, 2, 3), # Paper
+    (2, 3, 1), # Scissors
+)
+
+
 """
     parse_day2(inp_str)
 
@@ -16,43 +29,21 @@ Each match is represented as an tuple of `Hand` enums.
 function parse_day2(inp_str)
     matches = eachsplit(inp_str, "\n")
     map(matches) do match
-        (Char(match[1]), Char(match[3]))
+        (Char(match[1]) - 'A' + 1, Char(match[3]) - 'X' + 1)
     end |> collect
 end
 
-function to_hand(num)
-    @match num begin
-        0 => Rock
-        1 => Paper
-        2 => Scissors
-    end
-end
-
 function day2_part1(inp)
-    games = map(inp) do match
-        (to_hand(match[1] - 'A'), to_hand(match[2] - 'X'))
+    sum(inp) do match
+        game_scores[match[1]][match[2]] + match[2]
     end
-    sum(game_score, games) 
 end
 
 
 # TODO: Implement (Char, Char) -> Game mapping according to Part 2
 function day2_part2(inp)
-end
-
-function game_score(game)
-    hand_score(game[2]) + @match game begin
-        (Rock, Paper) => 6
-        (Paper, Scissors) => 6
-        (Scissors, Rock) => 6
-        (h1, h2) => h1 == h2 ? 3 : 0
-    end
-end
-
-function hand_score(h)
-    @match h begin
-        Rock => 1
-        Paper => 2
-        Scissors => 3
+    sum(inp) do match
+        hand = hand_map[match[1]][match[2]]
+        game_scores[match[1]][hand] + hand
     end
 end
