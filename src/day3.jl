fae233ea482eda0fab1342b96d22cc4ef015e9a5
Wolf Thomsen <git@wolthom.com>
2022-12-05 23:37:19 +0100

    make day3 solution more idiomatic (but slightly less performant)

--- src/day3.jl
+++ src/day3.jl
@@ -10,33 +10,15 @@ function priority(item)
 end
 
 function day3_part1(inp)
-    comp1, comp2 = Set{Char}(), Set{Char}()
-    out = 0
-    for rucksack in inp
+    map(inp) do rucksack
         idx = div(length(rucksack), 2)
         @views p1, p2 = (rucksack[begin:idx], rucksack[idx+1:end])
-
-        union!(comp1, p1)
-        union!(comp2, p2)
-        
-        out += intersect(comp1, comp2) |> only |> priority
-        empty!(comp1)
-        empty!(comp2)
-    end
-    out
+        intersect(p1, p2) |> only |> priority
+    end |> sum
 end
 
 function day3_part2(inp)
-    items = Set{Char}()
-    out = 0
-    for rucksacks in Iterators.partition(inp, 3)
-        (r1, r2, r3) = rucksacks
-
-        union!(items, r1)
-        intersect!(items, r2, r3)
-
-        out += only(items) |> priority
-        empty!(items)
-    end
-    out
+    map(Iterators.partition(inp, 3)) do rucksacks
+        intersect(rucksacks...) |> only |> priority
+    end |> sum
 end
