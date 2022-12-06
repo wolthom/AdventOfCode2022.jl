7b2de24a082f6e295eed1a2e90bd7d98cdcd0da4
Wolf Thomsen <git@wolthom.com>
2022-12-06 04:17:08 +0100

    set up structure for day 5

--- src/AdventOfCode2022.jl
+++ src/AdventOfCode2022.jl
@@ -17,4 +17,7 @@ export parse_day3, day3_part1, day3_part2
 include("./day4.jl")
 export parse_day4, day4_part1, day4_part2
 
+include("./day5.jl")
+export parse_day5, day5_part1, day5_part2
+
 end # module AdventOfCode2022
---
+++ src/day5.jl
@@ -0,0 +1,8 @@
+function parse_day5(inp_str)
+end
+
+function  day5_part1(inp)
+end
+
+function  day5_part2(inp)
+end
