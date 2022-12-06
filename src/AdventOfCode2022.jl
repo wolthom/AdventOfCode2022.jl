0f4c18aa770f4faad741495598a3504ed0b10cae
Wolf Thomsen <git@wolthom.com>
2022-12-06 02:23:31 +0100

    export day 4 methods

--- src/AdventOfCode2022.jl
+++ src/AdventOfCode2022.jl
@@ -14,4 +14,7 @@ export parse_day2, day2_part1, day2_part2
 include("./day3.jl")
 export parse_day3, day3_part1, day3_part2
 
+include("./day4.jl")
+export parse_day4, day4_part1, day4_part2
+
 end # module AdventOfCode2022
