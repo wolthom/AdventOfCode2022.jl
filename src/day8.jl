b6cab7a3936468a7a357fa637a6670c781a5aa2a
Wolf Thomsen <git@wolthom.com>
2022-12-10 19:32:11 +0100

    implement parsing for day 8

--- src/day8.jl
+++ src/day8.jl
@@ -1,4 +1,8 @@
 function parse_day8(inp_str)
+    vecs = map(eachsplit(chomp(inp_str), '\n')) do line
+        parse.(Int8, collect(line))
+    end
+    reduce(hcat, vecs)' |> collect
 end
 
 function day8_part1(inp)
