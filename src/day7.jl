439f645fdd508f5477361bf64ebcdd375f89ed9f
Wolf Thomsen <git@wolthom.com>
2022-12-10 13:29:00 +0100

    reimplement parsing into proper graph structure

--- src/day7.jl
+++ src/day7.jl
@@ -1,78 +1,107 @@
-struct FileEntry{T}
-    path::T
+using IterTools
+
+function name end
+
+struct Dir{T}
+    name::Union{String, SubString{String}}
+    children::Vector{T}
+end
+
+function name(d::Dir)
+    d.name
+end
+
+function entry_size(d::Dir)
+    sum(entry_size, d.children)
+end
+
+function entry_size(d::Dir, cache::AbstractDict)
+    dir_size = 0
+    for child in d.children
+        # Handle File-children
+        if child isa File 
+            dir_size += entry_size(child)
+            continue
+        end
+
+        # Handle Dir-children
+        child_name = name(child)
+        if haskey(cache, child_name)
+            dir_size += cache[child_name]
+            continue
+        end
+        child_size = entry_size(child, cache)
+        cache[child_name] = child_size
+
+        dir_size += child_size
+    end
+    dir_size
+end
+
+
+function make_root()
+    Dir("/", Vector{FsEntry}())
+end
+
+function find_dir(children, dir_name)
+    idx = findfirst(children) do entry
+        name(entry) |> endswith(dir_name)
+    end
+    isnothing(idx) && throw(InvalidStateException("cd into non-existing child directory", :Dir))
+    children[idx]
+end
+
+
+struct File
+    name::Union{String, SubString{String}}
     size::Int64
 end
 
-function path(e::FileEntry)
-    e.path
+const FsEntry = Union{Dir, File}
+
+function name(f::File)
+    f.name
 end
 
-struct DirEntry{T}
-    path::T
+function entry_size(f::File)
+    f.size
 end
-
-function path(d::DirEntry)
-    d.path
-end
-
-const FsEntry = Union{FileEntry, DirEntry}
 
 function parse_day7(inp_str)
-    cur_path = "/"
-    entries = Vector{FsEntry}()
-    push!(entries, DirEntry("/"))
+    root = make_root()
+
+    dir_stack = Vector{Dir}()
+    push!(dir_stack, root)
+
     for line in eachsplit(inp_str, '\n')
-        # Update current path if cd is encountered
+        cur_dir = dir_stack[end]
+
+        # '$ cd ..' => Move to parent
+        if line == "\$ cd .."
+            pop!(dir_stack)
+            continue
+        end
+
+        # '$ cd DIR' => Move to subdirectory
         if startswith(line, "\$ cd")
-            cur_path = parse_cd(cur_path, line)
+            dir_name = split(line, ' '; limit=3)[end]
+            dir_name == name(cur_dir) && continue
+
+            push!(dir_stack, find_dir(cur_dir.children, dir_name))
             continue
         end
 
-        # Handle ls output
+        # '$ ls' => Skip ls-command
         startswith(line, "\$ ls") && continue
-        push!(entries, parse_ls_line(cur_path, line))
-    end
-    sort!(entries; by=path)
-    entries
-end
 
-function parse_cd(cur_path, line)
-    new_path = ""
-    if contains(line, "..")
-        new_path = rsplit(cur_path, '/')[begin]
-    else  
-        new_path = joinpath(cur_path, split(line)[end])
-    end
-    new_path == "" ? "/" : new_path
-end
-
-function parse_ls_line(cur_path, line)
-    p1, p2 = split(line, ' ')
-    p1 == "dir" && return DirEntry(joinpath(cur_path, p2))
-    FileEntry(joinpath(cur_path, p2), parse(Int64, p1))
-end
-
-function day7_part1(inp)
-    dir_sizes = Vector{Tuple{DirEntry, Int64}}()
-    for idx in eachindex(inp)
-        e = inp[idx]
-        if e isa DirEntry
-            p = path(e)
-            same_path = x->(startswith(path(x), p))
-            sidx, eidx = (findfirst(same_path, inp), findlast(same_path, inp))
-            subarr = @view inp[sidx:eidx]
-            dir_size = Iterators.map(x->x.size, Iterators.filter(x->x isa FileEntry, subarr)) |> x -> sum(x; init=0)
-            0 < dir_size <= 100_000 && push!(dir_sizes, (e, dir_size)) 
+        # Parse ls output
+        if startswith(line, "dir")
+            dir_name = joinpath(name(cur_dir), split(line, ' '; limit=2)[end])
+            push!(cur_dir.children, Dir(dir_name, Vector{FsEntry}()))
         else
-            continue
+            size, file_name = split(line, " "; limit=2)
+            push!(cur_dir.children, File(joinpath(name(cur_dir), file_name), parse(Int64, size)))
         end
     end
-    # sort!(dir_sizes; by=x->x[2])
-    # dir_sizes
-    sum(dir_sizes) do el
-        el[2]
-    end
-end
-
-function day7_part2(inp)
+    return root
 end
