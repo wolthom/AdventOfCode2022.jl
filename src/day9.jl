cb75b9ea0922263ef4da165a343f47b1dc229f4e
Wolf Thomsen <git@wolthom.com>
2022-12-13 22:38:35 +0100

    implement day 9 parsing

--- src/day9.jl
+++ src/day9.jl
@@ -1,8 +1,28 @@
-function parse_day9(inp_str)
+using Accessors
+
+struct Pos
+    x::Int
+    y::Int
 end
 
-function day9_part1(inp)
+struct Mov
+    dir::Tuple{Int, Int}
+    steps::Int64
 end
+
+function parse_day9(inp_str)
+    map(to_mov, eachsplit(inp_str, '\n')) |> collect
+end
+
+function to_mov(line)
+    c = line[1]
+    dir = c == 'R' ? (1, 0) :
+            c == 'L' ? (-1, 0) :
+            c == 'U' ? (0, 1) : (0, -1)
+    steps = parse(Int64, split(line, ' ')[2])
+    Mov(dir, steps)
+end
+
 
 function day9_part2(inp)
 end
