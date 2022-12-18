950d5ca0ac9d63e3e61674586a633bf52210ec37
Wolf Thomsen <git@wolthom.com>
2022-12-18 19:49:46 +0100

    define data structures for day 14

--- src/day14.jl
+++ src/day14.jl
@@ -1,3 +1,18 @@
+struct LineSegment
+    sidx::CartesianIndex{2}
+    eidx::CartesianIndex{2}
+end
+
+Base.min(l::LineSegment) = min(l.sidx, l.eidx)
+Base.max(l::LineSegment) = max(l.sidx, l.eidx)
+
+struct Line
+    segments::Vector{LineSegment}
+end
+
+Base.minimum(l::Line) = reduce(min, l.segments)
+Base.maximum(l::Line) = reduce(min, l.segments)
+
 function parse_day14(inp_str)
 end
 
