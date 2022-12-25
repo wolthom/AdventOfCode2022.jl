d6891357b6ef1aec51e3f082ca74a591060c9769
Wolf Thomsen <git@wolthom.com>
2022-12-26 00:01:51 +0100

    optimize day 18 by using OffsetArrays and an actual Matrix for volume elements

--- src/day18.jl
+++ src/day18.jl
@@ -1,4 +1,6 @@
-const Point3D = Tuple{Int, Int, Int}
+using OffsetArrays
+
+@enum CubeType Unknown Air Obsidian
 
 const NEIGHBOR_DELTAS = CartesianIndex.((
     (1, 0, 0),
@@ -11,30 +13,6 @@ const NEIGHBOR_DELTAS = CartesianIndex.((
 
 const cube_line_regex = r"(\d*),(\d*),(\d*)"
 
-function parse_day18(inp_str)
-    cubes = Set{CartesianIndex{3}}()
-    foreach(eachsplit(chomp(inp_str), '\n')) do line
-        x_str, y_str, z_str = match(cube_line_regex, line)
-        x, y, z = (parse(Int, x_str), parse(Int, y_str), parse(Int, z_str))
-        push!(cubes, CartesianIndex(x, y, z))
-    end
-    cubes
-end
-
-function sides(cube, cubes)
-    6 - sum(NEIGHBOR_DELTAS) do delta
-        (cube + delta) ∈ cubes
-    end
-end
-
-function day18_part1(cubes)
-    exposed_sides = 0
-    for cube in cubes
-        exposed_sides += sides(cube, cubes)
-    end
-    exposed_sides
-end
-
 function get_boundaries(cubes)
     min_coord = CartesianIndex(ntuple(x -> typemax(Int), Val(3)))
     max_coord = CartesianIndex(ntuple(x -> typemin(Int), Val(3)))
@@ -46,49 +24,82 @@ function get_boundaries(cubes)
     (min_coord - i, max_coord + i)
 end
 
-function add_neighbors!(neighbors, cur_coord, box, air_cubes, cubes)
+function parse_day18(inp_str)
+    cubes = CartesianIndex{3}[]
+    foreach(eachsplit(chomp(inp_str), '\n')) do line
+        x_str, y_str, z_str = match(cube_line_regex, line)
+        x, y, z = (parse(Int, x_str), parse(Int, y_str), parse(Int, z_str))
+        push!(cubes, CartesianIndex(x, y, z))
+    end
+
+    # Fill UInt8 matrix that denotes type of volume
+    (min_coord, max_coord) = get_boundaries(cubes)
+    volume = fill(Unknown, Tuple(max_coord - min_coord) .+ (1, 1, 1))
+    volume = OffsetArray(volume, min_coord:max_coord)
+    for cube in cubes
+        volume[cube] = Obsidian
+    end
+
+    (cubes, volume)
+end
+
+function sides(cube, volume)
+    6 - sum(NEIGHBOR_DELTAS) do delta
+        idx = (cube + delta)
+        volume[idx] == Obsidian
+    end
+end
+
+function day18_part1((cubes, volume))
+    exposed_sides = 0
+    for cube in cubes
+        exposed_sides += sides(cube, volume)
+    end
+    exposed_sides
+end
+
+function add_neighbors!(neighbors, cur_coord, volume)
     for delta in NEIGHBOR_DELTAS
         new_coord = cur_coord + delta
+
         # Skip invalid or already seen points
-        cur_coord ∉ box && continue
-        (new_coord ∈ cubes || new_coord ∈ air_cubes) && continue
+        checkbounds(Bool, volume, new_coord) || continue
+        volume[new_coord] != Unknown && continue
 
         # Otherwise add point to list to be visited
         push!(neighbors, new_coord)
     end
 end
 
-function flood_fill(min_coord, max_coord, cubes)
-    air_cubes = Set{CartesianIndex{3}}()
-    box = min_coord:max_coord
-    cur_coord = min_coord
+function flood_fill!(volume)
+    cur_coord = findfirst(==(Unknown), volume)
 
     neighbors = CartesianIndex{3}[]
-    add_neighbors!(neighbors, cur_coord, box, air_cubes, cubes)
+    add_neighbors!(neighbors, cur_coord, volume)
+
     while length(neighbors) >= 1
-        push!(air_cubes, cur_coord)
+        volume[cur_coord] = Air
         cur_coord = pop!(neighbors) 
-        add_neighbors!(neighbors, cur_coord, box, air_cubes, cubes)
+        add_neighbors!(neighbors, cur_coord, volume)
     end
-
-    air_cubes 
 end
 
-function sides_p2(cube, air_cubes)
+function sides_p2(cube, volume)
     sum(NEIGHBOR_DELTAS) do delta
-        (cube + delta) ∈ air_cubes
+        idx = (cube + delta)
+        volume[idx] == Air
     end
 end
 
-function day18_part2(cubes)
-    # Determine non-enclosed air cubes
-    (min_coord, max_coord) = CartesianIndex.(get_boundaries(cubes))
-    air_cubes = flood_fill(min_coord, max_coord, cubes)
+function day18_part2((cubes, volume))
+    volume = copy(volume)
+    # Flood-fill air area
+    flood_fill!(volume)
 
     # Calculate exposed sides based on air cubes
     exposed_sides = 0
     for cube in cubes
-        exposed_sides += sides_p2(cube, air_cubes)
+        exposed_sides += sides_p2(cube, volume)
     end
 
     exposed_sides
