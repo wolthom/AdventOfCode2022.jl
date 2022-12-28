4f3566e941879b82a59a698b7361bb9bebdc7269
Wolf Thomsen <git@wolthom.com>
2022-12-28 23:40:31 +0100

    add calculation of upper bound for day 19

--- src/day19.jl
+++ src/day19.jl
@@ -2,7 +2,7 @@ using Accessors
 
 @enum Resources Ore=1 Clay=2 Obsidian=3 Geode=4
 
-const RobotCost = NTuple{4, Int16}
+const RobotCost = NTuple{3, Int16}
 
 struct BluePrint
     id::Int16
@@ -12,16 +12,18 @@ end
 const BLUEPRINT_REGEX = r"(\d+)"
 
 struct MiningState
-    resources::NTuple{4, Int16}
-    robots::NTuple{4, Int16}
-    mins::Int16
+    resources::NTuple{3, Int16}
+    robots::NTuple{3, Int16}
+    geodes::Int
+    mins::Int
 end
 
 function MiningState()
     MiningState(
-        (0, 0, 0, 0),
-        (1, 0, 0, 0), # 1 Ore robot at the beginning
-        24
+        (0, 0, 0),
+        (1, 0, 0), # 1 Ore robot at the beginning
+        0, 
+        24,
     )
 end
 
@@ -32,52 +34,18 @@ function parse_day19(inp_str)
         BluePrint(
             nums[1],
             (
-             (nums[2], 0, 0, 0), # Ore robot cost
-             (nums[3], 0, 0, 0), # Clay robot cost
-             (nums[4], nums[5], 0, 0), # Obsidian robot cost
-             (nums[6], 0, nums[7], 0), # Geode robot cost
+             (nums[2], 0, 0), # Ore robot cost
+             (nums[3], 0, 0), # Clay robot cost
+             (nums[4], nums[5], 0), # Obsidian robot cost
+             (nums[6], 0, nums[7]), # Geode robot cost
             )
         )
     end |> collect
 end
 
-function max_achievable(state)
-    # At most all geode robots + one robot per remaining minute can generate geodes
-    state.resources[Int(Geode)] +
-        state.robots[Int(Geode)] * state.mins +
-        ceil(Int, state.mins * (state.mins - 1) / 2)
-end
-
-function can_build(resources, cost)
-    all(resources .>= cost)
-end
-
-function still_required(robots, costs, robot)
-    # Geode robots are always needed
-    if robot == 4 
-        # println("Resource: $robot always required")
-        return true 
-    end
-    
-    # The robot is still needed if any resource requirements are higher than the current number
-    num_producers = robots[robot]
-    @inbounds for idx in 1:length(costs)
-        cost = costs[idx][robot]  
-        # @show num_producers, cost
-        if num_producers < cost
-            # println("Resource: $robot Still needed for $idx")
-            return true
-        end
-    end
-
-    false
-end
-
-
 function upper_bound(state, costs)
-    # Extract locally relevant variables
     cost = costs[Int(Geode)][Int(Obsidian)]
-    geodes = state.resources[Int(Geode)]
+    geodes = state.geodes
     obsidian = state.resources[Int(Obsidian)]
     rate = state.robots[Int(Obsidian)]
 
@@ -91,14 +59,16 @@ function upper_bound(state, costs)
         end
     end
     
-    res = reduce(reducer, state.mins:-1:0; init=(geodes, obsidian, rate))
+    # Apply for remaining time
+    res = reduce(reducer, (state.mins-1):-1:0; init=(geodes, obsidian, rate))
     res[1]
 end
 
 function is_relevant(move_idx, state, costs, max_costs, max_geodes)
-    return true
     # Check if robot is still required
-    state.robots[move_idx] >= max_costs[move_idx] && return false
+    move_idx != 4 && state.robots[move_idx] >= max_costs[move_idx] && return false
+
+    return true
        
     # Check if move can actually outperform current max
     # Smart approximation of the upper bound:
@@ -115,26 +85,37 @@ function calculate_next_state(move_idx, state, costs)
     out::Union{Nothing, MiningState} = nothing
 
     resource_eltype = eltype(state.resources)
+    geodes = state.geodes
     rates = state.robots
     robot_costs = costs[move_idx]
     
-    required_mins = resource_eltype(0)
-    for idx in 1:3 # Geodes are never a resource
-        cost = robot_costs[idx]
-        rate = rates[idx]
-
-        ((cost != 0) && (rate == 0)) && return out
-        rate == 0 && continue
-
-        required_mins = max(required_mins, ceil(resource_eltype, cost / rate))
+    # Bail if a required resource is not being produced
+    for idx in 1:length(robot_costs)
+        ((robot_costs[idx] != 0) && (rates[idx] == 0)) && return out
     end
-    required_mins > state.mins && return out
 
-    new_resources = state.resources .+ required_mins .* rates
+    # Find timepoint when sufficient resources are available
+    new_resources = state.resources
     new_robots = state.robots
-    @reset new_robots[move_idx] = new_robots[move_idx] + resource_eltype(1)
+    steps = resource_eltype(0)
+    while any(new_resources .< robot_costs)
+        new_resources = new_resources .+ rates
+        steps += resource_eltype(1)
+    end
+
+    # Bail if timepoint is after end
+    steps >= state.mins && return out
+
+    new_resources = new_resources .- robot_costs
+
+    # Whenever a new geode robot is manufactured, directly add its entire production
+    if move_idx == Int(Geode)
+        geodes += (state.mins - steps)
+    else
+        @reset new_robots[move_idx] = new_robots[move_idx] + resource_eltype(1)
+    end
    
-    out = MiningState(new_resources, new_robots, state.mins - required_mins)
+    out = MiningState(new_resources .+ rates, new_robots, geodes, state.mins - steps - 1)
 
     out
 end
@@ -145,8 +126,7 @@ function run_simulation!(start_state, bp)
     max_len = 0
     num_states = 0
 
-    max_costs = NTuple{4, Int16}(maximum(x->x[i], bp.costs) for i in 1:4)
-    @reset max_costs[4] = typemax(Int16) # There is no such thing as too many geodes
+    max_costs = NTuple{3, Int16}(maximum(x->x[i], bp.costs) for i in 1:3)
 
     states = MiningState[] 
     push!(states, start_state)
@@ -155,13 +135,13 @@ function run_simulation!(start_state, bp)
         # Retrieve current branch to explore
         state = pop!(states)
         num_states += 1
-        max_geodes = max(max_geodes, state.resources[Int(Geode)])
+        max_geodes = max(max_geodes, state.geodes)
         max_len = max(max_len, length(states))
 
         # Add branches to explore
         # TODO: Optimize by 'skipping ahead' for moves
         #       I.e. calculate when this move may be taken and directly calculate that state
-        for move_idx in 1:4 
+        for move_idx in Int.((Ore, Clay, Obsidian, Geode))
             next_state = calculate_next_state(move_idx, state, bp.costs)
             # Skip if desired state is unreachable
             isnothing(next_state) && continue
