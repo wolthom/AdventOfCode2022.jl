e7060d0f47d57091d794c92130aa69e8523906db
Wolf Thomsen <git@wolthom.com>
2023-01-06 16:20:20 +0100

    implement day 22 part 1

--- src/day22.jl
+++ src/day22.jl
@@ -2,22 +2,34 @@ using Accessors
 
 @enum Field Empty Floor Rock
 
-@enum Orientation Top=1 Left=2 Bottom=3 Right=4
+@enum Orientation Right=1 Bottom=2 Left=3 Top=4
 
 const Movement = Union{Char, Int}
 
 const MOVEMENT_REGEX = r"(\d+|\w)"
 
-function show_field(field)
+function show_field(field, pos)
+    # Get marker for position
+    marker = pos.or == Right ? '>' :
+             pos.or == Bottom ? 'v' :
+             pos.or == Left ? '<' : '^'
+
     out = IOBuffer();
-    for row in eachrow(field)
-        for el in row
+    # Write grove map
+    for (i, row) in enumerate(eachrow(field))
+        for (j, el) in enumerate(row)
+            if CartesianIndex(i, j) == pos.idx
+                write(out, marker)
+                continue
+            end
             c = el == Floor ? '.' :
                 el == Rock ? '#' : ' '
             write(out, c)
         end
         write(out, '\n')
     end
+
+    # Output final grove map
     s = String(take!(out))
     println(s)
 end
@@ -58,18 +70,22 @@ function parse_day22(inp_str)
 end
 
 struct Pos
-    x::Int
-    y::Int
+    idx::CartesianIndex{2}
     or::Orientation
 end
 
 function first_pos(field)
-    Pos(size(field, 1), 1, Right)
+    x_idx = 1
+    line = @view(field[x_idx, begin:end])
+    y_idx = findfirst(==(Floor), line)
+    isnothing(y_idx) && throw(DomainError("No valid field found in first line"))
+
+    Pos(CartesianIndex(x_idx, y_idx), Right)
 end
 
 function rotate(pos, dir)
     # Calculate new orientation
-    dir_val = dir == 'R' ? -1 : 1
+    dir_val = dir == 'R' ? 1 : -1
     or_val = Int(pos.or)
     new_or_val = mod1(or_val + dir_val, 4)
 
@@ -77,15 +93,52 @@ function rotate(pos, dir)
     @set pos.or = Orientation(new_or_val)
 end
 
+function shift(pos, field)
+    # Determine direction to move in
+    delta = pos.or == Top ? CartesianIndex(-1, 0) :
+            pos.or == Right ? CartesianIndex(0, 1) :
+            pos.or == Bottom ? CartesianIndex(1, 0) : CartesianIndex(0, -1)
+
+    # Determine coordinate delta in case of wrap-around
+    wrap = pos.or == Top ? CartesianIndex(size(field, 1), 0) :
+            pos.or == Right ? CartesianIndex(0, -size(field, 2)) :
+            pos.or == Bottom ? CartesianIndex(-size(field, 1), 0) : CartesianIndex(0, size(field, 2))
+    
+    # Calculate new index including wrap-around
+    new_idx = pos.idx + delta
+    new_idx += (1 - checkbounds(Bool, field, new_idx)) * wrap
+    field[new_idx] != Empty && return @set pos.idx = new_idx
+
+    # Keep moving if current field is empty
+    while (field[new_idx] == Empty)
+        new_idx = new_idx + delta
+        new_idx += (1 - checkbounds(Bool, field, new_idx)) * wrap
+    end
+    @set pos.idx = new_idx
+end
+
 function apply_movement(pos, mov, field)
     # Short circuit orientation change
     if mov isa Char
-        return rotate(Pos, mov)
+        return rotate(pos, mov)
     end
+
+    # Retrieve scanline for current movement
+    prev_pos = pos
+    for _ in 1:mov
+        new_pos = shift(prev_pos, field)
+        field[new_pos.idx] == Rock && return prev_pos
+        prev_pos = new_pos
+    end
+    prev_pos
 end
 
-function day22_part1(inp)
-
+function day22_part1((field, movements))
+    pos = first_pos(field)
+    for mov in movements
+        pos = apply_movement(pos, mov, field)
+    end
+    1000 * pos.idx[1] + 4 * pos.idx[2] + Int(pos.or) - 1
 end
 
 function day22_part2(inp)
